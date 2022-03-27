8d7c3dc8a806a59ef93c80c3228d17aa7aaa485a
Michael Callahan <mcallahan0@yahoo.com>
2022-03-26 22:02:38 -0400

    Added multiple IBOutlet connections between the view and the controller.

--- Clima/Controllers/WeatherViewController.swift
+++ Clima/Controllers/WeatherViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
